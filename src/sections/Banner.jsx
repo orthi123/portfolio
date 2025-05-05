10b8a05d91201d83caeb18c7251b14091d476a0b
orthi123 <orthi123@gmail.com>
2025-05-05 22:47:54 +0600

    Banner Section complited without responsive

--- src/sections/Banner.jsx
+++ src/sections/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { ReactTyped } from "react-typed";
 import Container from "../components/Container";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithubSquare } from "react-icons/fa";
@@ -18,7 +19,14 @@ const Banner = () => {
               Hi I am
             </h5>
             <h4 className="text-[#959595] font-bold text-[28px] mb-6">
-              ORTHI DEB SITHI
+              <ReactTyped
+                strings={["ORTHI DEB SITHI"]}
+                typeSpeed={40}
+                backSpeed={50}
+                loop
+              >
+                <input type="text" />
+              </ReactTyped>
             </h4>
             <h1 className="bg-gradient-to-r from-[rgb(152,67,0)] via-[rgb(253,111,0)] to-[rgb(202,89,0)] bg-clip-text text-transparent font-black text-[70px]">
               Frontend Developer
@@ -62,14 +70,38 @@ const Banner = () => {
               />
             </Flex>
           </div>
-          <div className="relative">
+          <div>
             <img
               src={BannerImg}
-              className="w-[520px] h-[543px]"
+              className="w-[520px] h-[543px] bannerShadow"
               alt=""
             />
-
-            {/* <img src={BannerShade} alt="" className="absolute top-[200px] right-[130px] w-[400px] h-[400px]" /> */}
+          </div>
+        </Flex>
+        <Flex className="bg-[rgb(255,255,255,4%)] w-[534px] h-[185px] mb-5 rounded-2xl items-center justify-around font-secondary_font">
+          <div className="border-r-2 border-[#959595]">
+            <h5 className="ml-5 text-primary-text font-extrabold text-[24px]">
+              5+
+            </h5>
+            <h6 className="ml-5 text-[#DFDFDF] font-bold text-[20px] mr-4">
+              Experiences
+            </h6>
+          </div>
+          <div className="border-r-2 border-[#959595]">
+            <h5 className="ml-5 text-primary-text font-extrabold text-[24px]">
+              20+
+            </h5>
+            <h6 className="ml-5 text-[#DFDFDF] font-bold text-[20px] mr-4">
+              Project done
+            </h6>
+          </div>
+          <div>
+            <h5 className="ml-5 text-primary-text font-extrabold text-[24px]">
+              80+
+            </h5>
+            <h6 className="ml-5 text-[#DFDFDF] font-bold text-[20px] mr-4">
+              Happy Clients
+            </h6>
           </div>
         </Flex>
       </Container>
